a56b6a83e1381cdb00c881168bbe7fad030369a9
Caline <calineyammine123@gmail.com>
2022-02-09 20:57:34 +0200

    Commented code

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
-let api1btn=document.getElementById("api1submit");
+let api1btn=document.getElementById("api1submit");   //variables used in first api, calculateAPI
 let calculation_result=document.getElementById("api1result");
 let first_number=document.getElementById("first_number");
 let second_number=document.getElementById("second_number");
 
-let api2btn=document.getElementById("api2submit");
+let api2btn=document.getElementById("api2submit");    //variables used in second api, removeExtraSpacesAPI
 let remove_space_result=document.getElementById("api2result");
 let string=document.getElementById("string");
 
-let api3btn=document.getElementById("api3submit");
+let api3btn=document.getElementById("api3submit");    //variables used in third api, palindromeAPI
 let is_palindrome_result=document.getElementById("api3result");
 let string_palindrome=document.getElementById("string_palindrome");
 
-let api4btn=document.getElementById("api4submit");
+let api4btn=document.getElementById("api4submit");    //variables used in fourth api, passwordAPI
 let password_result=document.getElementById("api4result");
 let password=document.getElementById("password");
 
@@ -24,7 +24,7 @@ api4btn.addEventListener("click",isSecure);
 async function calculate(){
     let x=first_number.value;
     let y=second_number.value;
-    if (x==='' || y===''){
+    if (x==='' || y===''){  //check if no input is given
         calculation_result.textContent="Please fill both numbers.";
         return;
     }
@@ -54,6 +54,7 @@ async function isPalindrome(){
     res=await response.json();
     is_palindrome_result.textContent="Result: "+ res.result;
 }
+
 async function isSecure(){
     let my_str=password.value;
     if (my_str===''){
@@ -62,6 +63,6 @@ async function isSecure(){
     }
     const response=await fetch(`http://localhost/APIs_project/PHP/passwordAPI.php?password=${my_str}`);
     res=await response.json();
-    password_result.innerHTML="Result: "+ res.passedTest+"<br>";
-    password_result.innerHTML=password_result.innerHTML+"Hashed version: "+ res.hashed;
+    password_result.innerHTML="Result: "+ res.passedTest+"<br>";          //display if the password is secure, contains minimum 8 letters/numbers
+    password_result.innerHTML=password_result.innerHTML+"Hashed version: "+ res.hashed; //displayed hashed version of the password
 }
