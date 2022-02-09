71c2068c249827ea475a6cd96f1f99f5402e0eed
Caline <calineyammine123@gmail.com>
2022-02-09 12:36:24 +0200

    Created the remove extra spaces API

---
+++ PHP/removeExtraSpacesAPI.php
@@ -0,0 +1,11 @@
+<?php
+
+$my_str=$_GET['string'];
+$trimmed_str_at_start = ltrim($my_str); //remove spaces from start
+$trimmed_str_at_end = rtrim($trimmed_str_at_start); //remove spaces from end
+$string= preg_replace('/\s\s+/', ' ', $trimmed_str_at_end); //replace two or more spaces with one space
+
+$array=['string'=>$my_str,'result'=>$string];
+echo json_encode($array);
+
+?>
